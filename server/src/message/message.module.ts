c5df15e518d4b36017be94020a083cf021e1ec91
Baptiste <b.girard@ttge.fr>
2024-06-18 14:34:22 +0200

    AHHH

--- server/src/message/message.module.ts
+++ server/src/message/message.module.ts
@@ -1,7 +1,7 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { MemoryService } from '../memory/memory.service';
 import { ConversationResolver } from '../conversation/conversation.resolver';
+import { MemoryService } from '../memory/memory.service';
 import { MessageConsumer } from './message.consumer';
 
 @Module({
